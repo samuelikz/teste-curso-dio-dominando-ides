3d08d7cf891857021ca58a8d991b3045d183a328
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2021-10-16 13:33:38 -0300

    Add files via upload

---
+++ src/sac/com/model/Gato.java
@@ -0,0 +1,15 @@
+package sac.com.model;
+
+public class Gato {
+	private String nome;
+	private String cor;
+	private Integer idade;
+	
+	public Gato() {}
+	
+	public Gato(String nome, String cor, Integer idade){
+			this.nome = nome;
+			this.cor = cor;
+			this.idade = idade;
+		}
+}
---
+++ src/sac/com/model/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package sac.com.model;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2000;
+		int b = 21;
+		System.out.println("Hello World! > "+ (a+b));
+	}
+
+}
